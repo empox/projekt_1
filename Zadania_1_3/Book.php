335a187f95dbf049b6385f16d6ea7359b40a7268
empox <empox@o2.pl>
2017-11-23 13:08:30 +0100

    Zadanie 1 v2

--- Zadania_1_3/Book.php
+++ Zadania_1_3/Book.php
@@ -14,8 +14,8 @@
 /*
  *Zadanie 1
 
- Zrobić klasę Book posiadajcą pole title - dostęp private
- Zrobić 3 różne klasy rozszerzajece klasę Book (JAVABook, PHPBook, NETBook)
+ 1. Zrobić klasę Book posiadajcą pole title - dostęp private
+ 2. Zrobić 3 różne klasy rozszerzajece klasę Book (JAVABook, PHPBook, NETBook)
  wszystkie 3 podklasy mają construktor bezparametrowy uzupełniając nadklasę 
  Book o tytuł program ma możliwość w sposób 
  minimalny umożliwić wypisania zawartości  pola title
@@ -23,28 +23,40 @@
 
 class Book {
     private $title;
-
+    
+    public function __construct($title){
+        $this->title=$title;
+        echo 'Nadano mi tytuł: '.$this->title.'<br>';
+        }
+    
 }
+    
+
 
 class JAVAbook extends Book{
-    public function __construct(){
-        $this->title = "ksiazka o javie";
-    
+    public function __construct()
+            {
+        parent::__construct('JAVAbook');
+   echo "utworzyłem ksiazke o JAVA <br><br>"   ;                                                                                                  
 }
 }
 
 class PHPBook extends Book{
-    public function __construct(){
-        $this->title = "ksiazka o PHP";
-        
-    }
-    
+    public function __construct()
+            {
+        parent::__construct('PHPbook');
+   echo "utworzyłem ksiazke o PHP <br><br>"   ;   
+}
 }
 
 class NETBook extends Book{
     public function __construct(){
-        $this->title = "ksiazka o dotNet";
+   parent::__construct('NEtbook');
+   echo "utworzyłem ksiazke o PHP <br><br>"   ; 
     }
    
 }
 
+$java=new JAVAbook();
+$php=new PHPBook();
+$net=new NETBook();
