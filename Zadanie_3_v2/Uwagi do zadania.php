86e5da5059797548947530acb550f01c0f3dae71
empox <empox@o2.pl>
2017-12-15 14:42:30 +0100

    wszelka krytyka mile widziana :P

--- Zadanie_3_v2/Uwagi do zadania.php
+++ Zadanie_3_v2/Uwagi do zadania.php
@@ -3,3 +3,4 @@ bede to jeszcze zmienial i probowa l ogarnac, poki co probuje napisac to i owo
 a co bedzie pozniej tam w ciele tych metod to uzupelnie pozniej
 
 czy tutaj chodzi o implementacje wzorca fabryki? :P
+wszelka krytyka mile widziana :P
