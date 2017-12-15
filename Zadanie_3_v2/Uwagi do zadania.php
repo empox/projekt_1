83917b61bc3c7fbcfc90aa0623b4acfcdfdff73e
TomMannson <tomasz.krol88@gmail.com>
2017-12-15 15:46:17 +0100

    Update Uwagi do zadania.php

--- Zadanie_3_v2/Uwagi do zadania.php
+++ Zadanie_3_v2/Uwagi do zadania.php
@@ -3,4 +3,6 @@ bede to jeszcze zmienial i probowa l ogarnac, poki co probuje napisac to i owo
 a co bedzie pozniej tam w ciele tych metod to uzupelnie pozniej
 
 czy tutaj chodzi o implementacje wzorca fabryki? :P
+// Tak Abstrakcyjnej Nawet, ale nie tylko :D
 wszelka krytyka mile widziana :P
+// dodałem ci też komentarz w Factorio
